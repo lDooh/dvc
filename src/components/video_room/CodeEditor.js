e3b44b07aad6627e065fe6326efd82750d83257d
Lee Doo Hee <dlengml0528@gmail.com>
2023-06-04 18:53:15 +0900

    #16 code concurrent editing

--- src/components/video_room/CodeEditor.js
+++ src/components/video_room/CodeEditor.js
@@ -1,5 +1,42 @@
-function CodeEditor() {
-    return <div>코드에디터</div>;
+import CodeMirror from "codemirror";
+import "codemirror/lib/codemirror.css";
+import "codemirror/mode/javascript/javascript";
+import { useEffect, useRef, useState } from "react";
+import { firebaseDatabase } from "../../firebaseInstance";
+
+function CodeEditor({ roomId }) {
+    const textareaRef = useRef(null);
+    const codeMirrorRef = useRef(null);
+    const [code, setCode] = useState("");
+
+    useEffect(() => {
+        codeMirrorRef.current = CodeMirror.fromTextArea(textareaRef.current, {
+            mode: "javascript",
+            theme: "default",
+            lineNumbers: true,
+        });
+
+        codeMirrorRef.current.on("change", (instance) => {
+            const value = instance.getValue();
+            const documentRef = firebaseDatabase.ref(`documents/${roomId}`);
+            documentRef.set({ value });
+        });
+
+        firebaseDatabase
+            .ref(`documents/${roomId}/value`)
+            .on("value", (snapshot) => {
+                const text = snapshot.val();
+                const cursor = codeMirrorRef.current.getCursor(); // 현재 커서 위치 저장
+                codeMirrorRef.current.setValue(text);
+                codeMirrorRef.current.setCursor(cursor); // 커서 위치 복원
+            });
+    }, []);
+
+    return (
+        <div>
+            <textarea ref={textareaRef} value={code} />
+        </div>
+    );
 }
 
 export default CodeEditor;
