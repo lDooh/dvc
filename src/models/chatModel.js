d4120ed3a6eb1737607353f397af566befd84e0a
Lee Doo Hee <dlengml0528@gmail.com>
2023-10-05 23:10:17 +0900

    Add function that get chatting record.

--- src/models/chatModel.js
+++ src/models/chatModel.js
@@ -1,6 +1,25 @@
 const db = require("../db");
 
 /**
+ * Return uid, message, time of chatting.
+ * @param {String} roomId
+ * @param {Function} callback
+ */
+function getRoomChatByRoomId(roomId, callback) {
+    const sql =
+        "SELECT uid, message, chat_time FROM room_chat WHERE room_id = ? ORDER BY chat_time";
+    const pool = db.pool;
+
+    pool.query(sql, [roomId], (err, results, fields) => {
+        if (err) {
+            return callback(err);
+        }
+
+        return callback(null, results);
+    });
+}
+
+/**
  * Save user's chat to db.
  * @param {String} uid
  * @param {String} roomId
@@ -22,4 +41,5 @@ function sendNewRoomChatting(uid, roomId, message, callback) {
 
 module.exports = {
     sendNewRoomChatting,
+    getRoomChatByRoomId,
 };
